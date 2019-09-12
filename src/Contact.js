caabb4aa84fbe86ec6fed97a589ca02d927da532
regalrr0 <ryan.r.regal@gmail.com>
2019-09-11 23:21:46 -0400

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return(
+            <div>
+                <h2>CONTACT</h2>
+                <p>Send a mail on the internet (thinking of calling it I-Mail)</p>
+                <p>sendtome@me.com</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
---
+++ src/Stuff.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Stuff extends Component {
+    render() {
+        return(
+            <div>
+                <h2>STUFF</h2>
+                <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+                <ol>
+                    <li>Thing 1</li>
+                    <li>Thing 2</li>
+                    <li>Thing 3</li>
+                    <li>Thing 4</li>
+                    <li>Thing 5</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+export default Stuff;
